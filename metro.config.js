5266ff4e62a1f1712f27c1240085bd197b736f6b
Preston <preston_Fournier@hotmail.com>
2024-04-30 09:15:44 -0500

    change port

--- metro.config.js
+++ metro.config.js
@@ -8,4 +8,12 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  */
 const config = {};
 
+const defaultConfig = getDefaultConfig(__dirname);
+
+const customConfig = {
+  server: {
+    port: 8082
+  }
+};
+
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);

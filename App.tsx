6c8c7e33745543362ac6f6829bc17e2b010c60d7
Megan U <megan@gagepiracy.com>
2024-05-02 13:26:00 -0500

    start of adding new files in the app.tsx

--- App.tsx
+++ App.tsx
@@ -3,13 +3,29 @@ import { Provider as PaperProvider } from 'react-native-paper';
 import Home from './src/pages/home';
 import Login from "./src/pages/login";
 import { theme } from './src/theme';
+import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator();
 
 
 const App = () => {
   return (
     <PaperProvider theme={theme}>
-          <Login />
-        </PaperProvider>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Login"
+                    component={Login}
+                    options={{headerShown: false}}
+                />
+                <Stack.Screen
+                    name="home"
+                    component={Home}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    </PaperProvider>
   );
 };
 

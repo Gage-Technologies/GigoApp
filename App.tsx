d62b78b539e8ba80409e6d3a5170866afb97ec3b
Preston <preston_Fournier@hotmail.com>
2024-05-06 09:08:45 -0500

    Fix merge conflict bug

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import CreateNewAccount from "./src/pages/createNewAccount"
 import { theme } from './src/theme';
 import {NavigationContainer} from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import BottomBar from './src/components/BottomBar';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,23 +18,14 @@ const App = () => {
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen
-                    name="ForgotPassword"
-                    component={ForgotPassword}
+                    name="home"
+                    component={Home}
                     options={{headerShown: false}}
                 />
                 <Stack.Screen
                     name="Login"
                     component={Login}
                     options={{headerShown: false}}
-                />
-                <Stack.Screen
-                    name="SignUp"
-                    component={CreateNewAccount}
-                    options={{headerShown: false}}
-                />
-                <Stack.Screen
-                    name="home"
-                    component={Home}
                 />
             </Stack.Navigator>
         </NavigationContainer>

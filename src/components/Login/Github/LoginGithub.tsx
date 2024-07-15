15d1996ced9bdad9aa694e667751f3e8e8e8f700
Megan U <megan@gagepiracy.com>
2024-07-15 11:44:18 -0500

    final IDE issue fixes

--- src/components/Login/Github/LoginGithub.tsx
+++ src/components/Login/Github/LoginGithub.tsx
@@ -1,71 +1,73 @@
-import React, { Component } from 'react';
-import { TouchableOpacity, View, Text } from 'react-native';
-import { openAuthSessionAsync, closeAuthSession, InAppBrowser } from 'react-native-inappbrowser-reborn';
+import React, {Component} from 'react';
+import {TouchableOpacity, Text} from 'react-native';
+import {InAppBrowser} from 'react-native-inappbrowser-reborn';
 import PropTypes from 'prop-types';
-import { toQuery } from './utils';  // Ensure this utility handles encoding URI components correctly
+import {toQuery} from './utils'; // Ensure this utility handles encoding URI components correctly
 
 class LoginGithub extends Component {
-    static propTypes = {
-        buttonText: PropTypes.string,
-        children: PropTypes.node,
-        clientId: PropTypes.string.isRequired,
-        onRequest: PropTypes.func,
-        onSuccess: PropTypes.func,
-        onFailure: PropTypes.func,
-        redirectUri: PropTypes.string.isRequired,
-        scope: PropTypes.string,
-        disabled: PropTypes.bool
-    }
+  static propTypes = {
+    buttonText: PropTypes.string,
+    children: PropTypes.node,
+    clientId: PropTypes.string.isRequired,
+    onRequest: PropTypes.func,
+    onSuccess: PropTypes.func,
+    onFailure: PropTypes.func,
+    redirectUri: PropTypes.string.isRequired,
+    scope: PropTypes.string,
+    disabled: PropTypes.bool,
+  };
 
-    static defaultProps = {
-        buttonText: 'Sign in with GitHub',
-        scope: 'user:email',
-        onRequest: () => {},
-        onSuccess: () => {},
-        onFailure: () => {}
-    }
+  static defaultProps = {
+    buttonText: 'Sign in with GitHub',
+    scope: 'user:email',
+    onRequest: () => {},
+    onSuccess: () => {},
+    onFailure: () => {},
+  };
 
-    onBtnClick = async () => {
-        console.log("made it here")
-        const { clientId, scope, redirectUri } = this.props;
-        console.log("1")
-        const query = toQuery({
-            client_id: clientId,
-            scope,
-            redirect_uri: redirectUri,
-            response_type: 'code'
-        });
-        console.log("2")
-        const url = `https://github.com/login/oauth/authorize?${query}`;
-        console.log("3")
+  onBtnClick = async () => {
+    console.log('made it here');
+    const {clientId, scope, redirectUri} = this.props;
+    console.log('1');
+    const query = toQuery({
+      client_id: clientId,
+      scope,
+      redirect_uri: redirectUri,
+      response_type: 'code',
+    });
+    console.log('2');
+    const url = `https://github.com/login/oauth/authorize?${query}`;
+    console.log('3');
 
-        this.props.onRequest();
-        console.log("4")
-        try {
-            console.log("5")
-            const result = await InAppBrowser.openAuth(url, redirectUri);
-            console.log("6")
-            if (result.type === 'success' && result.url) {
-                console.log("7: ", result.url)
-                this.props.onSuccess(result.url);
-            } else {
-                console.log("8")
-                throw new Error('Authentication was cancelled or failed');
-            }
-        } catch (error) {
-            console.log("9: ", error)
-            this.props.onFailure(error);
-        }
+    this.props.onRequest();
+    console.log('4');
+    try {
+      console.log('5');
+      const result = await InAppBrowser.openAuth(url, redirectUri);
+      console.log('6');
+      if (result.type === 'success' && result.url) {
+        console.log('7: ', result.url);
+        this.props.onSuccess(result.url);
+      } else {
+        console.log('8');
+        throw new Error('Authentication was cancelled or failed');
+      }
+    } catch (error) {
+      console.log('9: ', error);
+      this.props.onFailure(error);
     }
+  };
 
-    render() {
-        const { buttonText, children } = this.props;
-        return (
-            <TouchableOpacity onPress={this.onBtnClick} disabled={this.props.disabled}>
-                {children || <Text>{buttonText}</Text>}
-            </TouchableOpacity>
-        );
-    }
+  render() {
+    const {buttonText, children} = this.props;
+    return (
+      <TouchableOpacity
+        onPress={this.onBtnClick}
+        disabled={this.props.disabled}>
+        {children || <Text>{buttonText}</Text>}
+      </TouchableOpacity>
+    );
+  }
 }
 
 export default LoginGithub;
--- src/components/Login/Github/PopupWindow.tsx
+++ src/components/Login/Github/PopupWindow.tsx
@@ -1,86 +1,90 @@
 import {toParams, toQuery} from './utils';
 
 class PopupWindow {
-    private id: string;
-    private url: string;
-    private options: {};
-    private window: WindowProxy | null | undefined;
-    private promise: Promise<unknown> | undefined;
-    private _iid: number | null | undefined;
+  private id: string;
+  private url: string;
+  private options: {};
+  //@ts-ignore
+  private window: WindowProxy | null | undefined;
+  private promise: Promise<unknown> | undefined;
+  private _iid: number | null | undefined;
 
-    constructor(id: string, url: string, options = {}) {
-        this.id = id;
-        this.url = url;
-        this.options = options;
+  constructor(id: string, url: string, options = {}) {
+    this.id = id;
+    this.url = url;
+    this.options = options;
+  }
+
+  open() {
+    const {url, id, options} = this;
+
+    this.window = this.window.open(url, id, toQuery(options, ','));
+  }
+
+  close() {
+    this.cancel();
+    if (this.window === null || this.window === undefined) {
+      return;
     }
+    this.window.close();
+  }
 
-    open() {
-        const {url, id, options} = this;
+  poll() {
+    this.promise = new Promise((resolve, reject) => {
+      this._iid = this.window.setInterval(() => {
+        try {
+          const popup = this.window;
 
-        this.window = window.open(url, id, toQuery(options, ','));
-    }
+          if (!popup || popup.closed) {
+            this.close();
 
-    close() {
-        this.cancel();
-        if (this.window === null || this.window === undefined) {
-            return
+            reject(new Error('The popup was closed'));
+
+            return;
+          }
+
+          if (
+            popup.location.href === this.url ||
+            popup.location.pathname === 'blank'
+          ) {
+            return;
+          }
+
+          const params = toParams(popup.location.search.replace(/^\?/, ''));
+
+          resolve(params);
+
+          this.close();
+        } catch (error) {
+          /*
+           * Ignore DOMException: Blocked a frame with origin from accessing a
+           * cross-origin frame.
+           */
         }
-        this.window.close();
+      }, 500);
+    });
+  }
+
+  cancel() {
+    if (this._iid) {
+      this.window.clearInterval(this._iid);
+      this._iid = null;
     }
+  }
 
-    poll() {
-        this.promise = new Promise((resolve, reject) => {
-            this._iid = window.setInterval(() => {
-                try {
-                    const popup = this.window;
+  then(onSuccess: Function, onFailure: Function) {
+    // @ts-ignore
+    return this.promise.then(onSuccess, onFailure);
+  }
 
-                    if (!popup || popup.closed) {
-                        this.close();
+  static open(id: string, url: string, options: {}) {
+    const popup = new this(id, url, options);
 
-                        reject(new Error('The popup was closed'));
+    popup.open();
+    popup.poll();
 
-                        return;
-                    }
-
-                    if (popup.location.href === this.url || popup.location.pathname === 'blank') {
-                        return;
-                    }
-
-                    const params = toParams(popup.location.search.replace(/^\?/, ''));
-
-                    resolve(params);
-
-                    this.close();
-                } catch (error) {
-                    /*
-                     * Ignore DOMException: Blocked a frame with origin from accessing a
-                     * cross-origin frame.
-                     */
-                }
-            }, 500);
-        });
-    }
-
-    cancel() {
-        if (this._iid) {
-            window.clearInterval(this._iid);
-            this._iid = null;
-        }
-    }
-
-    then(onSuccess: Function, onFailure: Function) {
-        // @ts-ignore
-        return this.promise.then(onSuccess, onFailure);
-    }
-
-    static open(id: string, url: string, options: {}) {
-        const popup = new this(id, url, options);
-
-        popup.open();
-        popup.poll();
-
-        return popup;
-    }
+    return popup;
+  }
 }
 
 export default PopupWindow;
